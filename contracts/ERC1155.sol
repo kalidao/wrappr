f7059fb8786568479753abaf3a0bd84bbd8f46a3
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-07-31 14:28:37 -0400

    Update ERC1155.sol
    🥢

--- contracts/ERC1155.sol
+++ contracts/ERC1155.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity >=0.8.0;
+pragma solidity >=0.8.4;
 
 /// @notice A generic interface for a contract which properly accepts ERC-1155 tokens.
 /// @author Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/tokens/ERC1155.sol)
